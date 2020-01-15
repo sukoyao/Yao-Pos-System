32a4756313b0db947ad7df65c8e9465f31ad4ae5
Yao <sukoyao1101@gmail.com>
2020-01-15 14:13:12 +0800

    Add price method for Drink constructor

--- app.js
+++ app.js
@@ -4,5 +4,32 @@ function Drink (name, ice, sugar) {
   this.sugar = sugar
 }
 
-let greenTea = new Drink ('綠茶', '少冰', '微糖')
-console.log(greenTea) 
+Drink.prototype.price = function () {
+  switch (this.name) {
+    case '日月潭紅茶':
+    case '鹿谷烏龍茶':
+    case '四季春':
+      return 30
+    case '冬瓜檸檬':
+    case '紅茶拿鐵':
+    case '烏龍奶茶':
+      return 50
+    case '珍珠奶茶':
+    case '多多綠茶':  
+      return 55
+    default:
+      alert('沒有這個飲料')    
+  }
+}
+
+let blockTea = new Drink ('日月潭紅茶', '微冰', '微糖')
+console.log(blockTea)
+console.log(blockTea.price())
+
+let oolongMilkTea = new Drink ('烏龍奶茶', '去冰', '半糖')
+console.log(oolongMilkTea)
+console.log(oolongMilkTea.price())
+
+let bubbleMilkTea = new Drink ('珍珠奶茶', '半冰', '不加糖')
+console.log(bubbleMilkTea)
+console.log(bubbleMilkTea.price())
